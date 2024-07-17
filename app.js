81c45627a4ca142d89cc133e1c24f23098ba8cf0
GurTati <tatiana.gurova.j@gmail.com>
2024-07-17 19:13:05 +0100

    create 2 test

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 function fizzbuzz(number) {
     let text = ""
     for (let i=1; i <= number; i++) {
+        if (i % 3 === 0) {
+            text += 'Fizz'
+        } else if (i % 5 === 0) {
+            text += 'Buzz'
+        } else {
         text += i.toString()
+        }
         text += "\n"
     }
     text = text.trim()
--- app.test.js
+++ app.test.js
@@ -1,5 +1,17 @@
 const fizzbuzz = require('./app');
 
-test('separates rows with newline', () => {
-  expect(fizzbuzz(2)).toBe("1\n2");
+// test('separates rows with newline', () => {
+//   expect(fizzbuzz(2)).toBe("1\n2");
+// });
+
+// test('print out "Fizz" if number is divisible by 3', () => {
+//   expect(fizzbuzz(3)).toBe("1\n2\nFizz");
+// });
+
+// test('print out "Buzz" if number is divisible by 5', () => {
+//   expect(fizzbuzz(5)).toBe("1\n2\nFizz\n4\nBuzz");
+// });
+
+test('print out "Buzz" if number is divisible by 5', () => {
+  expect(fizzbuzz(5)).toBe("1\n2\nFizz\n4\nBuzz");
 });
